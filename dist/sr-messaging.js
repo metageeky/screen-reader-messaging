bf9ca3a5edae83d5c1583acbb73e6bd8398be27e
metageeky <the.kate.deibel@gmail.com>
2021-05-18 15:13:11 -0400

    still getting blank and repeat problem

--- dist/sr-messaging.js
+++ dist/sr-messaging.js
@@ -4,19 +4,21 @@ var ScreenReaderMessenger = (function() {
 	function createMessenger() {
 		let srm = new Object();
 		srm.globalTimer = null;
-		srm.speakRegion = null;
+		srm.speakRegion = createSpeakRegion(); //document.getElementById('speak');
 		srm.sendMessage = function(msg) {
 			if(srm.speakRegion === null) {
-				srm.speakRegion = createSpeakRegion();
+				srm.speakRegion = createSpeakRegion(msg + ' at create');
 			}
 			//reset the global timer
 			srm.globalTimer = clearTimeout(srm.globalTimer); 
 			//clear the live region's value
-			srm.speakRegion.innerText = '';
+			if(srm.speakRegion.innerText .length != 0)
+				srm.speakRegion.innerText = '';
 			//add text in.
 			srm.speakRegion.innerText = msg
 			//and remove after 5 seconds
 			srm.globalTimer = setTimeout(()=> {
+				console.log('clearing');
 				srm.speakRegion.innerText = '';
 			},5000);
 		};
@@ -24,11 +26,12 @@ var ScreenReaderMessenger = (function() {
 		return srm;
 	}
 	
-	function createSpeakRegion() {
+	function createSpeakRegion(msg = '') {
 		let e = document.createElement('DIV');
 		e.style.cssText = 'position: absolute !important; clip: rect(1px, 1px, 1px, 1px) !important; padding: 0 !important; border: 0 !important; height: 1px !important; width: 1px !important; overflow: hidden !important;'
 		e.setAttribute('aria-live','polite');
 		e.setAttribute('aria-atomic','true');
+		e.innerText = msg;
 		document.body.appendChild(e);
 		return e;
 	}
@@ -44,9 +47,15 @@ var ScreenReaderMessenger = (function() {
 })();
 
 window.onload = function() {
+	// initiate the messenger
+	let srm = ScreenReaderMessenger.getMessenger();
+	
+	
 	document.getElementById('speak-trigger').addEventListener('click', function(evt) {
 		let d = new Date();
 		let srm = ScreenReaderMessenger.getMessenger();
-		srm.sendMessage('Seconds are ' + d.getSeconds());
+		let x = d.getSeconds();
+		console.log(x);
+		srm.sendMessage('Seconds are ' + x);
 	})
 }
